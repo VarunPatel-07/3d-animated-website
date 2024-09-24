072bb1ed909b59a4a93f31acf6e97f37e07d070b
varun <VarunSureshkumarPatel07@gmail.com>
2024-09-24 20:12:41 +0530

    make a mager change by converting the images form png to webp, by this saved up to 46mb space and also done some responcive task

--- script.js
+++ script.js
@@ -9,9 +9,7 @@ function locomotive() {
 
   ScrollTrigger.scrollerProxy("#main", {
     scrollTop(value) {
-      return arguments.length
-        ? locoScroll.scrollTo(value, 0, 0)
-        : locoScroll.scroll.instance.scroll.y;
+      return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
     },
 
     getBoundingClientRect() {
@@ -23,9 +21,7 @@ function locomotive() {
       };
     },
 
-    pinType: document.querySelector("#main").style.transform
-      ? "transform"
-      : "fixed",
+    pinType: document.querySelector("#main").style.transform ? "transform" : "fixed",
   });
   ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
   ScrollTrigger.refresh();
@@ -46,306 +42,306 @@ window.addEventListener("resize", function () {
 
 function files(index) {
   var data = `
-    ./images/male0001.png
-    ./images/male0002.png
-    ./images/male0003.png
-    ./images/male0004.png
-    ./images/male0005.png
-    ./images/male0006.png
-    ./images/male0007.png
-    ./images/male0008.png
-    ./images/male0009.png
-    ./images/male0010.png
-    ./images/male0011.png
-    ./images/male0012.png
-    ./images/male0013.png
-    ./images/male0014.png
-    ./images/male0015.png
-    ./images/male0016.png
-    ./images/male0017.png
-    ./images/male0018.png
-    ./images/male0019.png
-    ./images/male0020.png
-    ./images/male0021.png
-    ./images/male0022.png
-    ./images/male0023.png
-    ./images/male0024.png
-    ./images/male0025.png
-    ./images/male0026.png
-    ./images/male0027.png
-    ./images/male0028.png
-    ./images/male0029.png
-    ./images/male0030.png
-    ./images/male0031.png
-    ./images/male0032.png
-    ./images/male0033.png
-    ./images/male0034.png
-    ./images/male0035.png
-    ./images/male0036.png
-    ./images/male0037.png
-    ./images/male0038.png
-    ./images/male0039.png
-    ./images/male0040.png
-    ./images/male0041.png
-    ./images/male0042.png
-    ./images/male0043.png
-    ./images/male0044.png
-    ./images/male0045.png
-    ./images/male0046.png
-    ./images/male0047.png
-    ./images/male0048.png
-    ./images/male0049.png
-    ./images/male0050.png
-    ./images/male0051.png
-    ./images/male0052.png
-    ./images/male0053.png
-    ./images/male0054.png
-    ./images/male0055.png
-    ./images/male0056.png
-    ./images/male0057.png
-    ./images/male0058.png
-    ./images/male0059.png
-    ./images/male0060.png
-    ./images/male0061.png
-    ./images/male0062.png
-    ./images/male0063.png
-    ./images/male0064.png
-    ./images/male0065.png
-    ./images/male0066.png
-    ./images/male0067.png
-    ./images/male0068.png
-    ./images/male0069.png
-    ./images/male0070.png
-    ./images/male0071.png
-    ./images/male0072.png
-    ./images/male0073.png
-    ./images/male0074.png
-    ./images/male0075.png
-    ./images/male0076.png
-    ./images/male0077.png
-    ./images/male0078.png
-    ./images/male0079.png
-    ./images/male0080.png
-    ./images/male0081.png
-    ./images/male0082.png
-    ./images/male0083.png
-    ./images/male0084.png
-    ./images/male0085.png
-    ./images/male0086.png
-    ./images/male0087.png
-    ./images/male0088.png
-    ./images/male0089.png
-    ./images/male0090.png
-    ./images/male0091.png
-    ./images/male0092.png
-    ./images/male0093.png
-    ./images/male0094.png
-    ./images/male0095.png
-    ./images/male0096.png
-    ./images/male0097.png
-    ./images/male0098.png
-    ./images/male0099.png
-    ./images/male0100.png
-    ./images/male0101.png
-    ./images/male0102.png
-    ./images/male0103.png
-    ./images/male0104.png
-    ./images/male0105.png
-    ./images/male0106.png
-    ./images/male0107.png
-    ./images/male0108.png
-    ./images/male0109.png
-    ./images/male0110.png
-    ./images/male0111.png
-    ./images/male0112.png
-    ./images/male0113.png
-    ./images/male0114.png
-    ./images/male0115.png
-    ./images/male0116.png
-    ./images/male0117.png
-    ./images/male0118.png
-    ./images/male0119.png
-    ./images/male0120.png
-    ./images/male0121.png
-    ./images/male0122.png
-    ./images/male0123.png
-    ./images/male0124.png
-    ./images/male0125.png
-    ./images/male0126.png
-    ./images/male0127.png
-    ./images/male0128.png
-    ./images/male0129.png
-    ./images/male0130.png
-    ./images/male0131.png
-    ./images/male0132.png
-    ./images/male0133.png
-    ./images/male0134.png
-    ./images/male0135.png
-    ./images/male0136.png
-    ./images/male0137.png
-    ./images/male0138.png
-    ./images/male0139.png
-    ./images/male0140.png
-    ./images/male0141.png
-    ./images/male0142.png
-    ./images/male0143.png
-    ./images/male0144.png
-    ./images/male0145.png
-    ./images/male0146.png
-    ./images/male0147.png
-    ./images/male0148.png
-    ./images/male0149.png
-    ./images/male0150.png
-    ./images/male0151.png
-    ./images/male0152.png
-    ./images/male0153.png
-    ./images/male0154.png
-    ./images/male0155.png
-    ./images/male0156.png
-    ./images/male0157.png
-    ./images/male0158.png
-    ./images/male0159.png
-    ./images/male0160.png
-    ./images/male0161.png
-    ./images/male0162.png
-    ./images/male0163.png
-    ./images/male0164.png
-    ./images/male0165.png
-    ./images/male0166.png
-    ./images/male0167.png
-    ./images/male0168.png
-    ./images/male0169.png
-    ./images/male0170.png
-    ./images/male0171.png
-    ./images/male0172.png
-    ./images/male0173.png
-    ./images/male0174.png
-    ./images/male0175.png
-    ./images/male0176.png
-    ./images/male0177.png
-    ./images/male0178.png
-    ./images/male0179.png
-    ./images/male0180.png
-    ./images/male0181.png
-    ./images/male0182.png
-    ./images/male0183.png
-    ./images/male0184.png
-    ./images/male0185.png
-    ./images/male0186.png
-    ./images/male0187.png
-    ./images/male0188.png
-    ./images/male0189.png
-    ./images/male0190.png
-    ./images/male0191.png
-    ./images/male0192.png
-    ./images/male0193.png
-    ./images/male0194.png
-    ./images/male0195.png
-    ./images/male0196.png
-    ./images/male0197.png
-    ./images/male0198.png
-    ./images/male0199.png
-    ./images/male0200.png
-    ./images/male0201.png
-    ./images/male0202.png
-    ./images/male0203.png
-    ./images/male0204.png
-    ./images/male0205.png
-    ./images/male0206.png
-    ./images/male0207.png
-    ./images/male0208.png
-    ./images/male0209.png
-    ./images/male0210.png
-    ./images/male0211.png
-    ./images/male0212.png
-    ./images/male0213.png
-    ./images/male0214.png
-    ./images/male0215.png
-    ./images/male0216.png
-    ./images/male0217.png
-    ./images/male0218.png
-    ./images/male0219.png
-    ./images/male0220.png
-    ./images/male0221.png
-    ./images/male0222.png
-    ./images/male0223.png
-    ./images/male0224.png
-    ./images/male0225.png
-    ./images/male0226.png
-    ./images/male0227.png
-    ./images/male0228.png
-    ./images/male0229.png
-    ./images/male0230.png
-    ./images/male0231.png
-    ./images/male0232.png
-    ./images/male0233.png
-    ./images/male0234.png
-    ./images/male0235.png
-    ./images/male0236.png
-    ./images/male0237.png
-    ./images/male0238.png
-    ./images/male0239.png
-    ./images/male0240.png
-    ./images/male0241.png
-    ./images/male0242.png
-    ./images/male0243.png
-    ./images/male0244.png
-    ./images/male0245.png
-    ./images/male0246.png
-    ./images/male0247.png
-    ./images/male0248.png
-    ./images/male0249.png
-    ./images/male0250.png
-    ./images/male0251.png
-    ./images/male0252.png
-    ./images/male0253.png
-    ./images/male0254.png
-    ./images/male0255.png
-    ./images/male0256.png
-    ./images/male0257.png
-    ./images/male0258.png
-    ./images/male0259.png
-    ./images/male0260.png
-    ./images/male0261.png
-    ./images/male0262.png
-    ./images/male0263.png
-    ./images/male0264.png
-    ./images/male0265.png
-    ./images/male0266.png
-    ./images/male0267.png
-    ./images/male0268.png
-    ./images/male0269.png
-    ./images/male0270.png
-    ./images/male0271.png
-    ./images/male0272.png
-    ./images/male0273.png
-    ./images/male0274.png
-    ./images/male0275.png
-    ./images/male0276.png
-    ./images/male0277.png
-    ./images/male0278.png
-    ./images/male0279.png
-    ./images/male0280.png
-    ./images/male0281.png
-    ./images/male0282.png
-    ./images/male0283.png
-    ./images/male0284.png
-    ./images/male0285.png
-    ./images/male0286.png
-    ./images/male0287.png
-    ./images/male0288.png
-    ./images/male0289.png
-    ./images/male0290.png
-    ./images/male0291.png
-    ./images/male0292.png
-    ./images/male0293.png
-    ./images/male0294.png
-    ./images/male0295.png
-    ./images/male0296.png
-    ./images/male0297.png
-    ./images/male0298.png
-    ./images/male0299.png
-    ./images/male0300.png
+    ./images/male0001.webp
+    ./images/male0002.webp
+    ./images/male0003.webp
+    ./images/male0004.webp
+    ./images/male0005.webp
+    ./images/male0006.webp
+    ./images/male0007.webp
+    ./images/male0008.webp
+    ./images/male0009.webp
+    ./images/male0010.webp
+    ./images/male0011.webp
+    ./images/male0012.webp
+    ./images/male0013.webp
+    ./images/male0014.webp
+    ./images/male0015.webp
+    ./images/male0016.webp
+    ./images/male0017.webp
+    ./images/male0018.webp
+    ./images/male0019.webp
+    ./images/male0020.webp
+    ./images/male0021.webp
+    ./images/male0022.webp
+    ./images/male0023.webp
+    ./images/male0024.webp
+    ./images/male0025.webp
+    ./images/male0026.webp
+    ./images/male0027.webp
+    ./images/male0028.webp
+    ./images/male0029.webp
+    ./images/male0030.webp
+    ./images/male0031.webp
+    ./images/male0032.webp
+    ./images/male0033.webp
+    ./images/male0034.webp
+    ./images/male0035.webp
+    ./images/male0036.webp
+    ./images/male0037.webp
+    ./images/male0038.webp
+    ./images/male0039.webp
+    ./images/male0040.webp
+    ./images/male0041.webp
+    ./images/male0042.webp
+    ./images/male0043.webp
+    ./images/male0044.webp
+    ./images/male0045.webp
+    ./images/male0046.webp
+    ./images/male0047.webp
+    ./images/male0048.webp
+    ./images/male0049.webp
+    ./images/male0050.webp
+    ./images/male0051.webp
+    ./images/male0052.webp
+    ./images/male0053.webp
+    ./images/male0054.webp
+    ./images/male0055.webp
+    ./images/male0056.webp
+    ./images/male0057.webp
+    ./images/male0058.webp
+    ./images/male0059.webp
+    ./images/male0060.webp
+    ./images/male0061.webp
+    ./images/male0062.webp
+    ./images/male0063.webp
+    ./images/male0064.webp
+    ./images/male0065.webp
+    ./images/male0066.webp
+    ./images/male0067.webp
+    ./images/male0068.webp
+    ./images/male0069.webp
+    ./images/male0070.webp
+    ./images/male0071.webp
+    ./images/male0072.webp
+    ./images/male0073.webp
+    ./images/male0074.webp
+    ./images/male0075.webp
+    ./images/male0076.webp
+    ./images/male0077.webp
+    ./images/male0078.webp
+    ./images/male0079.webp
+    ./images/male0080.webp
+    ./images/male0081.webp
+    ./images/male0082.webp
+    ./images/male0083.webp
+    ./images/male0084.webp
+    ./images/male0085.webp
+    ./images/male0086.webp
+    ./images/male0087.webp
+    ./images/male0088.webp
+    ./images/male0089.webp
+    ./images/male0090.webp
+    ./images/male0091.webp
+    ./images/male0092.webp
+    ./images/male0093.webp
+    ./images/male0094.webp
+    ./images/male0095.webp
+    ./images/male0096.webp
+    ./images/male0097.webp
+    ./images/male0098.webp
+    ./images/male0099.webp
+    ./images/male0100.webp
+    ./images/male0101.webp
+    ./images/male0102.webp
+    ./images/male0103.webp
+    ./images/male0104.webp
+    ./images/male0105.webp
+    ./images/male0106.webp
+    ./images/male0107.webp
+    ./images/male0108.webp
+    ./images/male0109.webp
+    ./images/male0110.webp
+    ./images/male0111.webp
+    ./images/male0112.webp
+    ./images/male0113.webp
+    ./images/male0114.webp
+    ./images/male0115.webp
+    ./images/male0116.webp
+    ./images/male0117.webp
+    ./images/male0118.webp
+    ./images/male0119.webp
+    ./images/male0120.webp
+    ./images/male0121.webp
+    ./images/male0122.webp
+    ./images/male0123.webp
+    ./images/male0124.webp
+    ./images/male0125.webp
+    ./images/male0126.webp
+    ./images/male0127.webp
+    ./images/male0128.webp
+    ./images/male0129.webp
+    ./images/male0130.webp
+    ./images/male0131.webp
+    ./images/male0132.webp
+    ./images/male0133.webp
+    ./images/male0134.webp
+    ./images/male0135.webp
+    ./images/male0136.webp
+    ./images/male0137.webp
+    ./images/male0138.webp
+    ./images/male0139.webp
+    ./images/male0140.webp
+    ./images/male0141.webp
+    ./images/male0142.webp
+    ./images/male0143.webp
+    ./images/male0144.webp
+    ./images/male0145.webp
+    ./images/male0146.webp
+    ./images/male0147.webp
+    ./images/male0148.webp
+    ./images/male0149.webp
+    ./images/male0150.webp
+    ./images/male0151.webp
+    ./images/male0152.webp
+    ./images/male0153.webp
+    ./images/male0154.webp
+    ./images/male0155.webp
+    ./images/male0156.webp
+    ./images/male0157.webp
+    ./images/male0158.webp
+    ./images/male0159.webp
+    ./images/male0160.webp
+    ./images/male0161.webp
+    ./images/male0162.webp
+    ./images/male0163.webp
+    ./images/male0164.webp
+    ./images/male0165.webp
+    ./images/male0166.webp
+    ./images/male0167.webp
+    ./images/male0168.webp
+    ./images/male0169.webp
+    ./images/male0170.webp
+    ./images/male0171.webp
+    ./images/male0172.webp
+    ./images/male0173.webp
+    ./images/male0174.webp
+    ./images/male0175.webp
+    ./images/male0176.webp
+    ./images/male0177.webp
+    ./images/male0178.webp
+    ./images/male0179.webp
+    ./images/male0180.webp
+    ./images/male0181.webp
+    ./images/male0182.webp
+    ./images/male0183.webp
+    ./images/male0184.webp
+    ./images/male0185.webp
+    ./images/male0186.webp
+    ./images/male0187.webp
+    ./images/male0188.webp
+    ./images/male0189.webp
+    ./images/male0190.webp
+    ./images/male0191.webp
+    ./images/male0192.webp
+    ./images/male0193.webp
+    ./images/male0194.webp
+    ./images/male0195.webp
+    ./images/male0196.webp
+    ./images/male0197.webp
+    ./images/male0198.webp
+    ./images/male0199.webp
+    ./images/male0200.webp
+    ./images/male0201.webp
+    ./images/male0202.webp
+    ./images/male0203.webp
+    ./images/male0204.webp
+    ./images/male0205.webp
+    ./images/male0206.webp
+    ./images/male0207.webp
+    ./images/male0208.webp
+    ./images/male0209.webp
+    ./images/male0210.webp
+    ./images/male0211.webp
+    ./images/male0212.webp
+    ./images/male0213.webp
+    ./images/male0214.webp
+    ./images/male0215.webp
+    ./images/male0216.webp
+    ./images/male0217.webp
+    ./images/male0218.webp
+    ./images/male0219.webp
+    ./images/male0220.webp
+    ./images/male0221.webp
+    ./images/male0222.webp
+    ./images/male0223.webp
+    ./images/male0224.webp
+    ./images/male0225.webp
+    ./images/male0226.webp
+    ./images/male0227.webp
+    ./images/male0228.webp
+    ./images/male0229.webp
+    ./images/male0230.webp
+    ./images/male0231.webp
+    ./images/male0232.webp
+    ./images/male0233.webp
+    ./images/male0234.webp
+    ./images/male0235.webp
+    ./images/male0236.webp
+    ./images/male0237.webp
+    ./images/male0238.webp
+    ./images/male0239.webp
+    ./images/male0240.webp
+    ./images/male0241.webp
+    ./images/male0242.webp
+    ./images/male0243.webp
+    ./images/male0244.webp
+    ./images/male0245.webp
+    ./images/male0246.webp
+    ./images/male0247.webp
+    ./images/male0248.webp
+    ./images/male0249.webp
+    ./images/male0250.webp
+    ./images/male0251.webp
+    ./images/male0252.webp
+    ./images/male0253.webp
+    ./images/male0254.webp
+    ./images/male0255.webp
+    ./images/male0256.webp
+    ./images/male0257.webp
+    ./images/male0258.webp
+    ./images/male0259.webp
+    ./images/male0260.webp
+    ./images/male0261.webp
+    ./images/male0262.webp
+    ./images/male0263.webp
+    ./images/male0264.webp
+    ./images/male0265.webp
+    ./images/male0266.webp
+    ./images/male0267.webp
+    ./images/male0268.webp
+    ./images/male0269.webp
+    ./images/male0270.webp
+    ./images/male0271.webp
+    ./images/male0272.webp
+    ./images/male0273.webp
+    ./images/male0274.webp
+    ./images/male0275.webp
+    ./images/male0276.webp
+    ./images/male0277.webp
+    ./images/male0278.webp
+    ./images/male0279.webp
+    ./images/male0280.webp
+    ./images/male0281.webp
+    ./images/male0282.webp
+    ./images/male0283.webp
+    ./images/male0284.webp
+    ./images/male0285.webp
+    ./images/male0286.webp
+    ./images/male0287.webp
+    ./images/male0288.webp
+    ./images/male0289.webp
+    ./images/male0290.webp
+    ./images/male0291.webp
+    ./images/male0292.webp
+    ./images/male0293.webp
+    ./images/male0294.webp
+    ./images/male0295.webp
+    ./images/male0296.webp
+    ./images/male0297.webp
+    ./images/male0298.webp
+    ./images/male0299.webp
+    ./images/male0300.webp
 `;
 
   return data.split("\n")[index];
@@ -386,17 +382,7 @@ function scaleImage(img, ctx) {
   var centerShift_x = (canvas.width - img.width * ratio) / 2;
   var centerShift_y = (canvas.height - img.height * ratio) / 2;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(
-    img,
-    0,
-    0,
-    img.width,
-    img.height,
-    centerShift_x,
-    centerShift_y,
-    img.width * ratio,
-    img.height * ratio
-  );
+  ctx.drawImage(img, 0, 0, img.width, img.height, centerShift_x, centerShift_y, img.width * ratio, img.height * ratio);
 }
 ScrollTrigger.create({
   trigger: "#page>canvas",
@@ -407,41 +393,41 @@ ScrollTrigger.create({
 });
 gsap.to("#page1", {
   scrollTrigger: {
-    trigger:`#page1`,
-    start:`top top`,
-    end:`bottom top`,
+    trigger: `#page1`,
+    start: `top top`,
+    end: `bottom top`,
     // markes: true,
-    pin:true,
-    scroller:`#main`,
+    pin: true,
+    scroller: `#main`,
   },
 });
 gsap.to("#page2", {
   scrollTrigger: {
-    trigger:`#page2`,
-    start:`top top`,
-    end:`bottom top`,
+    trigger: `#page2`,
+    start: `top top`,
+    end: `bottom top`,
     //   markes: true,
-    pin:true,
-    scroller:`#main`
+    pin: true,
+    scroller: `#main`,
   },
 });
 gsap.to("#page3", {
   scrollTrigger: {
-    trigger:`#page3`,
-    start:`top top`,
-    end:`bottom top`,
+    trigger: `#page3`,
+    start: `top top`,
+    end: `bottom top`,
     //   markes: true,
     pin: true,
-    scroller:`#main`
+    scroller: `#main`,
   },
 });
 gsap.to("#page4", {
   scrollTrigger: {
-    trigger:`#page4`,
-    start:`top top`,
-    end:`bottom top`,
+    trigger: `#page4`,
+    start: `top top`,
+    end: `bottom top`,
     //   markes: true,
     pin: true,
-    scroller:`#main`
+    scroller: `#main`,
   },
 });
